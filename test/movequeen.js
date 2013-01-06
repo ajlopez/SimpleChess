7f37bdfbe2ef4d2e685f87681a0cad5ce64034a8
ajlopez <webmaster@ajlopez.com>
2013-01-06 07:09:37 -0300

    Queen moves test

--- test/movequeen.js
+++ test/movequeen.js
@@ -4,39 +4,39 @@ var simplechess = require('../'),
 
 var White = simplechess.White;
 var Black = simplechess.Black;
-var Bishop = simplechess.Bishop;
+var Queen = simplechess.Queen;
 
 // First test
 
 var board = simplechess.createBoard();
-board.putContent(0, 0, { color: White, piece: Bishop });
+board.putContent(0, 0, { color: White, piece: Queen });
 
 var game = simplechess.createGame(board);
 
 var moves = game.getMoves(White);
 
 assert.ok(moves);
-assert.equal(moves.length, 7);
+assert.equal(moves.length, 21);
 
-// Blocking bishop
+// Blocking queen
 
-board.putContent(1, 1, { color: Black, piece: Bishop });
+board.putContent(1, 1, { color: Black, piece: Queen });
 var moves = game.getMoves(White);
 assert.ok(moves);
-assert.equal(moves.length, 1);
+assert.equal(moves.length, 15);
 
 var moves = game.getMoves(Black);
 assert.ok(moves);
-assert.equal(moves.length, 9);
+assert.equal(moves.length, 23);
 
 // New board
 
 var board = simplechess.createBoard();
 var game = simplechess.createGame(board);
-board.putContent(3, 3, { color: Black, piece: Bishop });
+board.putContent(3, 3, { color: Black, piece: Queen });
 var moves = game.getMoves(Black);
 assert.ok(moves);
-assert.equal(moves.length, 13);
+assert.equal(moves.length, 27);
 moves = game.getMoves(White);
 assert.ok(moves);
 assert.equal(moves.length, 0);

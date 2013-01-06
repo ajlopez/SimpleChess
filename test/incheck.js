34b422673fa657ac6bbc97602cee6cbe744e2477
ajlopez <webmaster@ajlopez.com>
2013-01-06 09:01:15 -0300

    Additional test for rook check, with fix

--- test/incheck.js
+++ test/incheck.js
@@ -4,6 +4,8 @@ var simplechess = require('../'),
 
 var White = simplechess.White;
 var Black = simplechess.Black;
+var King = simplechess.King;
+var Rook = simplechess.Rook;
 
 // No check in empty board
 
@@ -12,4 +14,45 @@ var game = simplechess.createGame(board);
 
 assert.equal(game.inCheck(White), false);
 assert.equal(game.inCheck(Black), false);
+
+// No check in only king is present
+
+var board = simplechess.createBoard();
+board.putContent(3, 3, { color: White, piece: King });
+var game = simplechess.createGame(board);
+
+assert.equal(game.inCheck(White), false);
+assert.equal(game.inCheck(Black), false);
+
+// Check if Rook attack
+
+var board = simplechess.createBoard();
+board.putContent(3, 3, { color: White, piece: King });
+board.putContent(3, 7, { color: Black, piece: Rook });
+var game = simplechess.createGame(board);
+
+assert.equal(game.inCheck(White), true);
+assert.equal(game.inCheck(Black), false);
+
+board.removeContent(3, 7);
+board.putContent(3, 0, { color: Black, piece: Rook });
+var game = simplechess.createGame(board);
+
+assert.equal(game.inCheck(White), true);
+assert.equal(game.inCheck(Black), false);
+
+board.removeContent(3, 0);
+board.putContent(0, 3, { color: Black, piece: Rook });
+var game = simplechess.createGame(board);
+
+assert.equal(game.inCheck(White), true);
+assert.equal(game.inCheck(Black), false);
+
+board.removeContent(0, 3);
+board.putContent(7, 3, { color: Black, piece: Rook });
+var game = simplechess.createGame(board);
+
+assert.equal(game.inCheck(White), true);
+assert.equal(game.inCheck(Black), false);
+
 

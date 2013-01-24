b4668294dfa22811dcaa21e44bf669a553ff944a
ajlopez <webmaster@ajlopez.com>
2013-01-24 07:17:53 -0300

    Remove unused file; game.putPieces with first test

--- test/game.js
+++ test/game.js
@@ -7,6 +7,9 @@ var simplechess = require('../'),
 assert.ok(simplechess.White);
 assert.ok(simplechess.Black);
 
+var White = simplechess.White;
+var Black = simplechess.Black;
+
 // Pieces defined
 
 assert.ok(simplechess.Rook);
@@ -40,3 +43,29 @@ for (var k in positions) {
 assert.equal(nwhite, 16);
 assert.equal(nblack, 16);
 
+// create game with empty board and put pieces
+
+var board = simplechess.createBoard();
+var game = simplechess.createGame(board);
+
+game.putPieces([{ x: 3, y: 3, color: White, piece: simplechess.Rook },
+    { x: 4, y: 4, color: Black, piece: simplechess.Bishop }]);
+    
+var positions = game.getPositions();
+
+assert.ok(positions);
+assert.equal(positions.length, 2);
+
+var nwhite = 0;
+var nblack = 0;
+
+for (var k in positions) {
+    var position = positions[k];
+    if (position.content.color === simplechess.White)
+        nwhite++;
+    else if (position.content.color == simplechess.Black)
+        nblack++;
+}
+
+assert.equal(nwhite, 1);
+assert.equal(nblack, 1);

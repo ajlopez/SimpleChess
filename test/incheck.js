17b16626bf6dcb2d47ba9cfc5529a4a8f7b136a8
ajlopez <webmaster@ajlopez.com>
2020-11-08 10:13:08 -0300

    In check test refactor

--- test/incheck.js
+++ test/incheck.js
@@ -1,40 +1,46 @@
 
-var simplechess = require('../'),
-    assert = require('assert');
+const simplechess = require('../');
+const    assert = require('assert');
 
-var White = simplechess.White;
-var Black = simplechess.Black;
-var King = simplechess.King;
-var Queen = simplechess.Queen;
-var Rook = simplechess.Rook;
-var Bishop = simplechess.Bishop;
-var Knight = simplechess.Knight;
-var Pawn = simplechess.Pawn;
+const White = simplechess.White;
+const Black = simplechess.Black;
+const King = simplechess.King;
+const Queen = simplechess.Queen;
+const Rook = simplechess.Rook;
+const Bishop = simplechess.Bishop;
+const Knight = simplechess.Knight;
+const Pawn = simplechess.Pawn;
 
 // No check in empty board
 
-var board = simplechess.createBoard();
-var game = simplechess.createGame(board);
+{
+    const board = simplechess.createBoard();
+    const game = simplechess.createGame(board);
 
-assert.equal(game.inCheck(White), false);
-assert.equal(game.inCheck(Black), false);
+    assert.equal(game.inCheck(White), false);
+    assert.equal(game.inCheck(Black), false);
+}
 
 // No check in only king is present
 
-var board = simplechess.createBoard();
-board.putContent(3, 3, { color: White, piece: King });
-var game = simplechess.createGame(board);
+{
+    const board = simplechess.createBoard();
+    
+    board.putContent(3, 3, { color: White, piece: King });
+    
+    const game = simplechess.createGame(board);
 
-assert.equal(game.inCheck(White), false);
-assert.equal(game.inCheck(Black), false);
+    assert.equal(game.inCheck(White), false);
+    assert.equal(game.inCheck(Black), false);
+}
 
 // Check if King at position x y, and it is attackbed by piece 
 
 function checkInCheck(piece, x, y) {
-    var board = simplechess.createBoard();
-    var game = simplechess.createGame(board);
+    let board = simplechess.createBoard();
+    let game = simplechess.createGame(board);
 
-    var moves = game.getPieceMoves(Black, Knight, x, y);
+    const moves = game.getPieceMoves(Black, Knight, x, y);
 
     assert.ok(moves);
     assert.ok(moves.length > 0);
@@ -49,27 +55,29 @@ function checkInCheck(piece, x, y) {
     });
 }
 
-checkInCheck(Knight, 3, 3);
-checkInCheck(Bishop, 3, 3);
-checkInCheck(Rook, 3, 3);
-checkInCheck(Queen, 3, 3);
+{
+    checkInCheck(Knight, 3, 3);
+    checkInCheck(Bishop, 3, 3);
+    checkInCheck(Rook, 3, 3);
+    checkInCheck(Queen, 3, 3);
+}
 
 // Check if King at position x y, and it is not attackbed by misplaced piece 
 
 function checkNotInCheck(piece, x, y) {
-    var board = simplechess.createBoard();
-    var game = simplechess.createGame(board);
+    let board = simplechess.createBoard();
+    let game = simplechess.createGame(board);
 
-    var moves = game.getPieceMoves(Black, Knight, x, y);
+    const moves = game.getPieceMoves(Black, Knight, x, y);
 
     assert.ok(moves);
     assert.ok(moves.length > 0);
 
     moves.forEach(function(move) {
         board = simplechess.createBoard();
-        var width = board.getWidth();
+        const width = board.getWidth();
         board.putContent(x, y, { color: White, piece: King });
-        var x2 = move.x2 + 1;
+        const x2 = move.x2 + 1;
         if (x2 >= width)
             x2 = 0;
         board.putContent(x2, move.y2, { color: Black, piece: Knight });
@@ -79,25 +87,30 @@ function checkNotInCheck(piece, x, y) {
     });
 }
 
-checkNotInCheck(Knight, 3, 3);
-checkNotInCheck(Bishop, 3, 3);
-checkNotInCheck(Rook, 3, 3);
+{
+    checkNotInCheck(Knight, 3, 3);
+    checkNotInCheck(Bishop, 3, 3);
+    checkNotInCheck(Rook, 3, 3);
+}
 
 // Pawn attack
 
-var board = simplechess.createBoard();
-board.putContent(3, 3, { color: White, piece: King });
-board.putContent(2, 4, { color: Black, piece: Pawn });
-var game = simplechess.createGame(board);
+{
+    const board = simplechess.createBoard();
+    board.putContent(3, 3, { color: White, piece: King });
+    board.putContent(2, 4, { color: Black, piece: Pawn });
+    const game = simplechess.createGame(board);
 
-assert.equal(game.inCheck(White), true);
-assert.equal(game.inCheck(Black), false);
+    assert.equal(game.inCheck(White), true);
+    assert.equal(game.inCheck(Black), false);
+}
 
-var board = simplechess.createBoard();
-board.putContent(3, 3, { color: White, piece: King });
-board.putContent(4, 4, { color: Black, piece: Pawn });
-var game = simplechess.createGame(board);
+{
+    const board = simplechess.createBoard();
+    board.putContent(3, 3, { color: White, piece: King });
+    board.putContent(4, 4, { color: Black, piece: Pawn });
+    const game = simplechess.createGame(board);
 
-assert.equal(game.inCheck(White), true);
-assert.equal(game.inCheck(Black), false);
-
+    assert.equal(game.inCheck(White), true);
+    assert.equal(game.inCheck(Black), false);
+}

ab2f65b62ce1c62750aa1428b6f0caf14148ec5c
ajlopez <webmaster@ajlopez.com>
2020-11-14 07:56:02 -0300

    Rook moves tests refactor

--- test/moverook.js
+++ test/moverook.js
@@ -1,51 +1,72 @@
 
-var simplechess = require('../'),
-    assert = require('assert');
+const simplechess = require('../');
+const assert = require('assert');
 
-var White = simplechess.White;
-var Black = simplechess.Black;
-var Rook = simplechess.Rook;
+const White = simplechess.White;
+const Black = simplechess.Black;
+const Rook = simplechess.Rook;
 
 // First test
+{
+    const board = simplechess.createBoard();
+    
+    board.putContent(0, 0, { color: White, piece: Rook });
 
-var board = simplechess.createBoard();
-board.putContent(0, 0, { color: White, piece: Rook });
+    const game = simplechess.createGame(board);
+    const moves = game.getMoves(White);
 
-var game = simplechess.createGame(board);
-
-var moves = game.getMoves(White);
-
-assert.ok(moves);
-assert.equal(moves.length, 14);
+    assert.ok(moves);
+    assert.equal(moves.length, 14);
+}
 
 // Blocking rook
 
-board.putContent(0, 1, { color: Black, piece: Rook });
-var moves = game.getMoves(White);
-assert.ok(moves);
-assert.equal(moves.length, 8);
+{
+    const board = simplechess.createBoard();
+    
+    board.putContent(0, 0, { color: White, piece: Rook });
+    board.putContent(0, 1, { color: Black, piece: Rook });
 
-var moves = game.getMoves(Black);
-assert.ok(moves);
-assert.equal(moves.length, 14);
+    const game = simplechess.createGame(board);
+    
+    const moves = game.getMoves(White);
+    
+    assert.ok(moves);
+    assert.equal(moves.length, 8);
 
-board.putContent(1, 0, { color: Black, piece: Rook });
-var moves = game.getMoves(White);
-assert.ok(moves);
-assert.equal(moves.length, 2);
+    const moves2 = game.getMoves(Black);
+    
+    assert.ok(moves2);
+    assert.equal(moves2.length, 14);
 
-var moves = game.getMoves(Black);
-assert.ok(moves);
-assert.equal(moves.length, 28);
+    board.putContent(1, 0, { color: Black, piece: Rook });
+    
+    const moves3 = game.getMoves(White);
+    
+    assert.ok(moves3);
+    assert.equal(moves3.length, 2);
+
+    const moves4 = game.getMoves(Black);
+    
+    assert.ok(moves4);
+    assert.equal(moves4.length, 28);
+}
 
 // New board
+{
+    const board = simplechess.createBoard();
+    const game = simplechess.createGame(board);
+    
+    board.putContent(3, 3, { color: Black, piece: Rook });
+    
+    const moves = game.getMoves(Black);
+    
+    assert.ok(moves);
+    assert.equal(moves.length, 14);
+    
+    const moves2 = game.getMoves(White);
+    
+    assert.ok(moves2);
+    assert.equal(moves2.length, 0);
+}
 
-var board = simplechess.createBoard();
-var game = simplechess.createGame(board);
-board.putContent(3, 3, { color: Black, piece: Rook });
-var moves = game.getMoves(Black);
-assert.ok(moves);
-assert.equal(moves.length, 14);
-moves = game.getMoves(White);
-assert.ok(moves);
-assert.equal(moves.length, 0);

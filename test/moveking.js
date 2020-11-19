dc2ac8cfe8caf980017acf3c7c0a4322342a4968
ajlopez <webmaster@ajlopez.com>
2020-11-19 07:45:23 -0300

    Fix king moves

--- test/moveking.js
+++ test/moveking.js
@@ -6,6 +6,7 @@ const White = simplechess.White;
 const Black = simplechess.Black;
 const King = simplechess.King;
 const Rook = simplechess.Rook;
+const Knight = simplechess.Knight;
 
 // First test
 
@@ -19,6 +20,20 @@ const Rook = simplechess.Rook;
 
     assert.ok(moves);
     assert.equal(moves.length, 3);
+}
+
+// In center
+
+{
+    const board = simplechess.createBoard();
+
+    board.putContent(4, 4, { color: White, piece: King });
+
+    const game = simplechess.createGame(board);
+    const moves = game.getMoves(White);
+
+    assert.ok(moves);
+    assert.equal(moves.length, 8);
 }
 
 // Blocking king
@@ -41,6 +56,50 @@ const Rook = simplechess.Rook;
     
     assert.ok(moves2);
     assert.equal(moves2.length, 14);
+}
+
+// Blocking king in center
+
+{
+    const board = simplechess.createBoard();
+
+    board.putContent(4, 4, { color: White, piece: King });
+
+    const game = simplechess.createGame(board);
+    
+    board.putContent(5, 5, { color: Black, piece: Knight });
+    
+    const moves = game.getMoves(White);
+    
+    assert.ok(moves);
+    assert.equal(moves.length, 8);
+
+    const moves2 = game.getMoves(Black);
+    
+    assert.ok(moves2);
+    assert.equal(moves2.length, 8);
+}
+
+// Blocking king in center with same color piece
+
+{
+    const board = simplechess.createBoard();
+
+    board.putContent(4, 4, { color: White, piece: King });
+
+    const game = simplechess.createGame(board);
+    
+    board.putContent(5, 5, { color: White, piece: Knight });
+    
+    const moves = game.getMoves(White);
+    
+    assert.ok(moves);
+    assert.equal(moves.length, 7 + 8);
+
+    const moves2 = game.getMoves(Black);
+    
+    assert.ok(moves2);
+    assert.equal(moves2.length, 0);
 }
 
 // New board

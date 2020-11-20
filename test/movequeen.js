0c167d67d8ccb18b6ed0ceb467f9a9d65d9f4dd7
ajlopez <webmaster@ajlopez.com>
2020-11-20 08:17:51 -0300

    Additional queen moves tests

--- test/movequeen.js
+++ test/movequeen.js
@@ -5,6 +5,7 @@ const assert = require('assert');
 const White = simplechess.White;
 const Black = simplechess.Black;
 const Queen = simplechess.Queen;
+const Pawn = simplechess.Pawn;
 
 // First test
 
@@ -18,6 +19,20 @@ const Queen = simplechess.Queen;
 
     assert.ok(moves);
     assert.equal(moves.length, 21);
+}
+
+// Queen at center
+
+{
+    const board = simplechess.createBoard();
+    
+    board.putContent(4, 4, { color: White, piece: Queen });
+
+    const game = simplechess.createGame(board);
+    const moves = game.getMoves(White);
+
+    assert.ok(moves);
+    assert.equal(moves.length, 27);
 }
 
 // Blocking queen
@@ -37,6 +52,25 @@ const Queen = simplechess.Queen;
 
     assert.ok(moves2);
     assert.equal(moves2.length, 23);
+}
+
+// Blocking queen with same color piece
+{
+    const board = simplechess.createBoard();
+    
+    board.putContent(0, 0, { color: White, piece: Queen });
+    board.putContent(1, 1, { color: White, piece: Pawn });
+
+    const game = simplechess.createGame(board);
+    const moves = game.getMoves(White);
+
+    assert.ok(moves);
+    assert.equal(moves.length, 16);
+
+    const moves2 = game.getMoves(Black);
+
+    assert.ok(moves2);
+    assert.equal(moves2.length, 0);
 }
 
 // New board

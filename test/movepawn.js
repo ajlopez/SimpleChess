8a143bbb514f1d610d1495688b6e2c193bb98357
ajlopez <webmaster@ajlopez.com>
2020-11-21 06:55:34 -0300

    Additional pawn moves tests

--- test/movepawn.js
+++ test/movepawn.js
@@ -6,7 +6,7 @@ const White = simplechess.White;
 const Black = simplechess.Black;
 const Pawn = simplechess.Pawn;
 
-// First test
+// First test white
 
 {
     const board = simplechess.createBoard();
@@ -18,6 +18,51 @@ const Pawn = simplechess.Pawn;
 
     assert.ok(moves);
     assert.equal(moves.length, 1);
+}
+
+// First test black
+
+{
+    const board = simplechess.createBoard();
+    board.putContent(0, 4, { color: Black, piece: Pawn });
+
+    const game = simplechess.createGame(board);
+
+    const moves = game.getMoves(Black);
+
+    assert.ok(moves);
+    assert.equal(moves.length, 1);
+}
+
+// Blocked
+
+{
+    const board = simplechess.createBoard();
+    board.putContent(0, 3, { color: White, piece: Pawn });
+    board.putContent(0, 4, { color: Black, piece: Pawn });
+
+    const game = simplechess.createGame(board);
+
+    const moves = game.getMoves(White);
+
+    assert.ok(moves);
+    assert.equal(moves.length, 0);
+}
+
+// Blocked piece same color
+
+{
+    const board = simplechess.createBoard();
+    board.putContent(0, 3, { color: White, piece: Pawn });
+    board.putContent(0, 4, { color: White, piece: Pawn });
+    board.putContent(0, 5, { color: Black, piece: Pawn });
+
+    const game = simplechess.createGame(board);
+
+    const moves = game.getMoves(White);
+
+    assert.ok(moves);
+    assert.equal(moves.length, 0);
 }
 
 // White at start position
@@ -32,6 +77,21 @@ const Pawn = simplechess.Pawn;
     
     assert.ok(moves);
     assert.equal(moves.length, 2);
+}
+
+// White at start position partial blocking
+
+{
+    const board = simplechess.createBoard();
+    const game = simplechess.createGame(board);
+    
+    board.putContent(1, 1, { color: White, piece: Pawn });
+    board.putContent(1, 3, { color: Black, piece: Pawn });
+    
+    const moves = game.getMoves(White);
+    
+    assert.ok(moves);
+    assert.equal(moves.length, 1);
 }
 
 // Black at start position
@@ -62,6 +122,24 @@ const Pawn = simplechess.Pawn;
     
     assert.ok(moves);
     assert.equal(moves.length, 3);
+}
+
+// White capture with blocking
+
+{
+    const board = simplechess.createBoard();
+    const game = simplechess.createGame(board);
+    
+    board.putContent(3, 3, { color: White, piece: Pawn });
+    board.putContent(3, 4, { color: White, piece: Pawn });
+    board.putContent(3, 5, { color: Black, piece: Pawn });
+    board.putContent(4, 4, { color: Black, piece: Pawn });
+    board.putContent(2, 4, { color: Black, piece: Pawn });
+    
+    const moves = game.getMoves(White);
+    
+    assert.ok(moves);
+    assert.equal(moves.length, 2);
 }
 
 // Black capture
